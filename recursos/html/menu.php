bb703cc5e3caa28509b6bafe2df50aeb1201b3c2
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-01-26 18:21:41 -0600

    cambios al front icono logo y menu

--- recursos/html/menu.php
+++ recursos/html/menu.php
@@ -11,21 +11,6 @@
   
   <div class="sidebar">
 
-  <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-      <div class="image">
-      <?php 
-        $numero = '4';
-        if (SEXO == 'f'){
-          $numero = '3';
-        } 
-      ?>
-        <img src="<?php echo RUTA_PROYECTO; ?>img/avatar<?=$numero?>.png" class="img-circle elevation-2" alt="User Image">
-      </div>
-      <div class="info">
-        <a class="d-block" ><?= NOMBRE_USUARIO ?></a>
-      </div>
-    </div>
-
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
