f673fcd0e83e427e263c76195c764cca754d562d
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-01-29 18:10:13 -0600

    Vista\Productos\agregarProducto

--- app/vistas/productos/agregarProducto.php
+++ app/vistas/productos/agregarProducto.php
@@ -6,12 +6,17 @@ $requiredClases = "required class='form-control  form-control-sm'";
 
 $datosProducto = $controlador->datosProducto;
 
+$num_pagina = 1;
+if (isset($_GET['pag'])){
+    $num_pagina = (int) $_GET['pag'];
+}
+
 ?>
 <br>
 <h2>Agregar Producto</h2>
 <br>
 <form autocomplete="off" role="form" method="POST"
-      action="<?php echo Redireccion::obtener('productos','agregarProductoBd',SESSION_ID) ?>">
+      action="<?php echo Redireccion::obtener('productos','agregarProductoBd',SESSION_ID)."&pag={$num_pagina}" ?>">
       <input name="id" value="<?= $datosProducto['productos_id']; ?>" type='hidden'>
     <div class="row">
         <div class=col-md-3>
@@ -28,7 +33,7 @@ $datosProducto = $controlador->datosProducto;
         <div class='col-md-12'></div>
         <div class=col-md-3>
             <div class='input-group mb-3'>
-                <input title='Cantidad a agregar' name='nuevaCantidadProducto' placeholder='Cantidad a agregar' <?= $requiredClases; ?> type='number'>
+                <input autofocus title='Cantidad a agregar' name='cantidadProducto' placeholder='Cantidad a agregar' <?= $requiredClases; ?> type='number'>
             </div>
         </div>
         <div class='col-md-12'></div>
