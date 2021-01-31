7ea70de76a2ede059f1a63f9caf8f5a3f2c35628
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-01-30 18:55:06 -0600

    Service\login ready

---
+++ public/service/login.php
@@ -0,0 +1,28 @@
+<?php
+
+$rutaBase = __DIR__.'/../../';
+require_once "{$rutaBase}app/config/requires.php"; 
+
+use App\clases\Autentificacion;
+use App\errores\Base AS ErrorBase;
+
+try {
+    $claseDatabase = 'App\\clases\\'.DB_TIPO.'\\Database';
+    $coneccion = new $claseDatabase();
+}catch (ErrorBase $e) {
+    print_r('Error al conectarce a la base de datos, favor de contactar al equipo de desarrollo');
+    exit;
+}
+
+$autentificacion = new Autentificacion($coneccion);
+
+try {
+    $resultado = $autentificacion->login();
+}catch (ErrorBase $e) {
+    $respuesta = json_encode(['sessionId' => null ,'msj' => $e->getMessage()]);
+    print_r($respuesta);
+    exit;
+}
+
+$respuesta = json_encode(['sessionId' => $resultado['sessionId'], 'msj' => 'login exitoso']);
+print_r($respuesta);
